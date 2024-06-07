7b1c70fb783fd52ad1f9f969ffd92222be581a7e
himumi <erliansyaha@gmail.com>
2024-06-08 06:22:23 +0900

    refactor: create #convert_to_indexes in Travails class

--- lib/travails.rb
+++ lib/travails.rb
@@ -55,4 +55,23 @@ class Travails
       current = recorder[current]
     end
   end
+
+  def convert_to_indexes(key)
+    letters = {
+      'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3,
+      'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7
+    }
+
+    numbers = {
+      '1' => 7, '2' => 6, '3' => 5, '4' => 4,
+      '5' => 3, '6' => 2, '7' => 1, '8' => 0
+    }
+
+    letter = letters[key[0]]
+    number = numbers[key[1]]
+
+    return nil if letter.nil? or number.nil?
+
+    [number, letter]
+  end
 end
