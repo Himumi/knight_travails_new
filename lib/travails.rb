d3581b93609d928b7c7dc325ab5e7cc74c6d4cf9
himumi <erliansyaha@gmail.com>
2024-06-07 21:57:00 +0900

    refactor: create #add_all_directions in Travails class

--- lib/travails.rb
+++ lib/travails.rb
@@ -9,7 +9,19 @@ class Travails
     ]
 
     return nil if paths[way].any? { |i| !i.between?(0, 7) }
-    
+
     paths[way]
+  end
+
+  def add_all_directions(source, recorder)
+    result = []
+
+    8.times do |way|
+      adjacent = direction(source, way)
+
+      result << [adjacent, source] if !adjacent.nil? and !recorder[adjacent]
+    end
+
+    result
   end
 end
